260a48a1c526f385ec3558cafab370b6af7c3c8f
Zohaib Asif <zohaibdotasif@gmail.com>
2025-08-06 06:35:54 -0700

    add github workflows

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
-  base: "/3d-portfolio"
+  base: "/3d-portfolio/"
 })
